42c2ac4e4aa835f293d3040e8ab936bed0134190
mkiry9669 <137688774+mkiry9669@users.noreply.github.com>
2023-07-04 19:26:59 +0100

    Add files via upload

---
+++ script copy.js
@@ -0,0 +1,9 @@
+window.addEventListener('DOMContentLoaded', function() {
+    // Dynamically load content using JavaScript
+    var contentDiv = document.getElementById('content');
+    var contentHTML = '<h2>The Cyrillic Alphabet</h2>';
+    contentHTML += '<p>The Cyrillic alphabet is an alphabetic writing system used in various languages, including Bulgarian, Russian, Ukrainian, and many others.</p>';
+    contentHTML += '<p>It was created in the First Bulgarian Empire during the 9th century AD at the Preslav Literary School in the First Bulgarian Empire by the Saints Cyril and Methodius. It is considered the most important early cultural creation of the First Bulgarian Empire and to all Slavs:.</p>';
+    contentHTML += '<p>Over time, the Cyrillic alphabet underwent modifications and became the standard writing system for Slavic languages. Today, it plays a crucial role in the cultural and linguistic identity of many countries.</p>';
+    contentDiv.innerHTML = contentHTML;
+});
